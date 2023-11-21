7702605c2eb35d9f723f60ebc28a2f9df272acf4
Jakub Szewc <jakub.szewc62@gmail.com>
2023-11-21 15:50:29 +0100

    some logic changes

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 const tablica = []
+console.log(tablica)
 const button = document.querySelector('.button')
 const zadanie = document.querySelector('.zadanie')
 const input = document.querySelector('.input')
 let id = 0
 button.addEventListener('click', event => {
   event.preventDefault()
-  tablica.push({
-    id: `${id + 1}`,
-    zadanie: `${input.value}`,
-    visible: true
-  })
-  id++
-  input.value = ''
-  console.log('click')
+  if (!input.value.trim() == '') {
+    const newLi = document.createElement('li')
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = 'Delete'
+    const newTask = {
+      id: id + 1,
+      zadanie: input.value,
+      visible: true
+    }
+    newLi.dataset.class = newTask.id
+    deleteButton.dataset.class = "usun"
+    tablica.push(newTask)
+    
+    id++
+    
+    input.value = ''
 
-  zadanie.innerHTML += `<li>${tablica[(id -= 1)].zadanie}</li>`
-  id += 1
+    newLi.textContent = tablica[(id -= 1)].zadanie
+    zadanie.appendChild(newLi)
+    zadanie.appendChild(deleteButton)
+
+    id += 1
+  }
 })
+
+function deleteTask () {
+  
+}
