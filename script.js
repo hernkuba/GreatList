b172be4de6b0f53aa69a64e07015784bd87f61b2
Jakub Szewc <jakub.szewc62@gmail.com>
2023-11-23 17:06:52 +0100

    Made logic updates

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const tablica = []
 console.log(tablica)
 const button = document.querySelector('.button')
-const zadanie = document.querySelector('.zadanie')
+const zadanie = document.querySelector('.orderedList')
+const taskDone = document.querySelector('.done')
 const input = document.querySelector('.input')
 let id = 0
 button.addEventListener('click', event => {
@@ -10,13 +11,15 @@ button.addEventListener('click', event => {
     const newLi = document.createElement('li')
     const deleteButton = document.createElement('button')
     deleteButton.textContent = 'Delete'
+    const doneButton = document.createElement('button')
+    doneButton.textContent = '✔'
     const newTask = {
       id: id + 1,
       zadanie: input.value,
-      visible: true
+      done: false
     }
     newLi.classList.add('orderedList__li')
-    deleteButton.classList.add('usun')
+    deleteButton.classList.add('deleteButton')
     tablica.push(newTask)
 
     id++
@@ -24,15 +27,25 @@ button.addEventListener('click', event => {
     input.value = ''
 
     newLi.textContent = tablica[(id -= 1)].zadanie
+    newLi.appendChild(deleteButton)
+    newLi.appendChild(doneButton)
     zadanie.appendChild(newLi)
-    zadanie.appendChild(deleteButton)
 
     id += 1
 
+    doneButton.addEventListener('click', event => {
+      event.preventDefault()
+      newLi.classList.toggle('taskDone')
+      if (newLi.classList.contains('taskDone')) {
+        taskDone.appendChild(newLi)
+      } else {
+        zadanie.appendChild(newLi)
+      }
+    })
+
     deleteButton.addEventListener('click', event => {
       event.preventDefault()
-      zadanie.removeChild(newLi)
-      zadanie.removeChild(deleteButton)
+      newLi.remove()
     })
   }
 })
